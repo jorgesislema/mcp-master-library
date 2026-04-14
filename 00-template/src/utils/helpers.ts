b6da54699a0bb135666ca56cc6b7bd0e3b69337f
jorgesislema <79588110+jorgesislema@users.noreply.github.com>
2026-04-14 15:00:10 -0500

    Create helpers.ts

---
+++ 00-template/src/utils/helpers.ts
@@ -0,0 +1,8 @@
+﻿export function formatDuration(milliseconds: number): string {
+	if (milliseconds < 1000) {
+		return `${milliseconds}ms`;
+	}
+
+	const seconds = milliseconds / 1000;
+	return `${seconds.toFixed(seconds >= 10 ? 0 : 1)}s`;
+}
